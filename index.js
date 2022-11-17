6d14e77e9b7d9ec896feb1dd12e4a8973bba355d
“c.b.olha” <c.b.olha@viseven.com>
2022-11-17 15:27:51 -0300

    added logic to render new cards

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-let firstCard = 10;
-let secondCard = 4;
 let cardsEl = document.querySelector("#cards");
-let sum = firstCard + secondCard;
 let sumEl = document.querySelector("#sumEl");
 let hasBlackJack = false;
 let message = "";
@@ -9,7 +6,22 @@ let messageEl = document.querySelector("#messageEl");
 const startGameButton = document.querySelector("#startGame");
 const newCardButton = document.querySelector("#newCard");
 
+const getRandomCard = () => {
+  return 5;
+}
+
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+
 const renderGame = () => {
+  cardsEl.textContent = "Cards: ";
+
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
   if (sum === 21) {
     hasBlackJack = true;
     message = "Wohoo! You've got Blackjack!";
@@ -21,7 +33,6 @@ const renderGame = () => {
 
   messageEl.textContent = message;
   sumEl.textContent = `Sum: ${sum}`;
-  cardsEl.textContent = `Cards: ${firstCard} - ${secondCard}`;
 };
 
 startGameButton.onclick = () => {
@@ -29,8 +40,9 @@ startGameButton.onclick = () => {
 };
 
 const drawNewCard = () => {
-  let newCard = 11;
-  sum = sum + newCard;
+  let newCard = getRandomCard();
+  sum += newCard;
+  cards.push(newCard);
   renderGame();
 };
 
